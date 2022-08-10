7caefae47170b91a397f83f3ca9776b4b51b174b
Thatcher Deyoe <tdeyoe2@gmail.com>
2022-08-10 13:00:53 -0400

    initating login system

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
+Route::get('/register', [UserController::class, 'create']);
+Route::get('/login', [UserController::class, 'index']);
